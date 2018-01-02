dd44f0305a4b5d0688d8c4aa0ce5fa8c17c31400
Andres Puente <andres.puente012@gmail.com>
2018-01-02 17:47:11 -0500

    removed sensative twilio info

--- src/main/java/com/twilio/SendSms.java
+++ src/main/java/com/twilio/SendSms.java
@@ -6,16 +6,16 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class SendSms {
-	public static final String ACCOUNT_SID = "ACd5c03dde5f56202f6422d1becf6662d3";
-	public static final String AUT_TOKEN = "651c689c81408b303876af7362f576b7";
+	public static final String ACCOUNT_SID = "********";
+	public static final String AUT_TOKEN = "*******";
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Twilio.init(ACCOUNT_SID, AUT_TOKEN);
 		
 		Message message = Message.creator(
-				new PhoneNumber("+15168301709"),
-				new PhoneNumber("+19172439262"),
+				new PhoneNumber("********"),
+				new PhoneNumber("********"),
 				"HEY PETRUSHKA").create();
 		
 		System.out.println(message.getSid());
